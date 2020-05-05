f442d613b2a55929ea6ded48ba025cb866b17af5
tytech038 <62194272+tytech038@users.noreply.github.com>
2020-05-05 17:45:09 -0500

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,18 @@
+echo Please enter the version number you want to
+echo download a server for.
+echo eg. 1.8, 1.15.2
+read vernum
+curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar -Xmx1G BuildTools.jar --rev $vernum
+echo Step 2
+echo Please enter the name of the spigot.jar file
+echo Example: spigot-1.15.2.jar
+echo If you do not know. Use spigot-(minecraft version number).jar
+echo MUST INCLUDE EXTENSION!
+read varname
+cp $varname spigot.jar
+rm $varname
+chmod +x start.sh
+rm README.md
+echo Done.
+
