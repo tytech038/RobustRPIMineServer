92ade69694cc08319fa207e766e216526d8a6d1f
tytech038 <62194272+tytech038@users.noreply.github.com>
2020-05-05 13:19:53 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ java -jar -Xmx1G BuildTools.jar
 cowsay Step 2
 echo Please enter the name of the spigot.jar file
 echo Example: spigot-1.15.2.jar
+echo If you do not know. Use spigot-(minecraft version number).jar
 echo MUST INCLUDE EXTENSION!
 read varname
 cp $varname spigot.jar
