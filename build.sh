442c66fa311cef6db63e3ef946928baad78ff410
tytech038 <62194272+tytech038@users.noreply.github.com>
2020-05-09 23:04:37 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ echo Please enter the version number you want to
 echo download a server for.
 echo eg. 1.8, 1.15.2
 read vernum
+curl -o start.sh https://raw.githubusercontent.com/tytech038/RobustUnixMineServer/master/start.sh
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar -Xmx1G BuildTools.jar --rev $vernum
 echo Step 2
