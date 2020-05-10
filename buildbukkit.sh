7cf0df302d94bdf665e2ed304249eb411f21b466
tytech038 <62194272+tytech038@users.noreply.github.com>
2020-05-09 23:11:18 -0500

    Update buildbukkit.sh

--- buildbukkit.sh
+++ buildbukkit.sh
@@ -4,6 +4,7 @@ echo eg. 1.8, 1.15.2
 echo type latest for latest version
 read vernum
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+curl -o startbukkit.sh https://raw.githubusercontent.com/tytech038/RobustUnixMineServer/master/startbukkit.sh
 java -jar -Xmx1G BuildTools.jar --compile craftbukkit --rev $vernum
 echo Step 2
 echo Please enter the name of the bukkit.jar file
@@ -11,8 +12,8 @@ echo Example: bukkit-1.15.2.jar
 echo If you do not know. Use spigot-latest minecraft version number.jar
 echo MUST INCLUDE EXTENSION!
 read varname
-cp $varname spigot.jar
+cp $varname bukkit.jar
 rm $varname
-chmod +x start.sh
+chmod +x startbukkit.sh
 rm README.md
 echo Done.
