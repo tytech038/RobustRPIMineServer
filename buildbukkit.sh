a412f49d9f24d1350b36b4f98382a08ab7d6ccdf
tytech038 <62194272+tytech038@users.noreply.github.com>
2020-05-09 23:08:57 -0500

    Update buildbukkit.sh

--- buildbukkit.sh
+++ buildbukkit.sh
@@ -6,8 +6,8 @@ read vernum
 curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar -Xmx1G BuildTools.jar --compile craftbukkit --rev $vernum
 echo Step 2
-echo Please enter the name of the spigot.jar file
-echo Example: spigot-1.15.2.jar
+echo Please enter the name of the bukkit.jar file
+echo Example: bukkit-1.15.2.jar
 echo If you do not know. Use spigot-latest minecraft version number.jar
 echo MUST INCLUDE EXTENSION!
 read varname
